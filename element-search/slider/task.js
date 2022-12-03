90999c9d56de575d47eca5c0ca92106ff9722c66
Margarita May Lee <ritochka.m_lee@yahoo.com>
2022-12-03 13:04:55 +0300

    Third task

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,6 +1,6 @@
 const imageElems = document.querySelectorAll(".slider__item");
 const sliderCount = imageElems.length;
-const dotElems = document.querySelectorAll(".slider__dot");
+const dotElems = document.querySelectorAll(".slider__dot_active");
 const leftButton = document.querySelector(".slider__arrow_prev");
 const rightButton = document.querySelector(".slider__arrow_next");
 
@@ -32,3 +32,41 @@ rightButton.addEventListener('click', function(){
     toggleActiveSlide(activeSlideIndex);
     return activeSlideIndex;
 });
+
+
+leftButton.addEventListener('click', slideOnClick); 
+rightButton.addEventListener('click', slideOnClick);
+function slideOnClick() {
+    let step = 1;
+    if (this === leftButton) {
+        step = -1;
+    }
+    for (let i = 0; i < sliderCount; i++) {
+        if (imageElems.item(i).classList.contains('slider__item_active') && dotElems.item(i).classList.contains('slider__dot_active')) {
+            imageElems.item(i).classList.toggle('slider__item_active');
+            dotElems.item(i).classList.toggle('slider__dot_active');
+            let newIdx = i + step;
+            if (newIdx < 0) {
+                newIdx = sliderCount - 1;
+            } 
+            else if (newIdx === sliderCount) {
+                newIdx = 0;
+            }
+            imageElems.item(newIdx).classList.toggle('slider__item_active');
+            dotElems.item(newIdx).classList.toggle('slider__dot_active');
+            return;
+        }
+    }
+}
+const countOfDots = dotElems.length;
+for (let k = 0; k < countOfDots; k++) {
+    dotElems.item(k).addEventListener('click', function() {
+        let activeDot = document.querySelector(".slider__dot_active");
+        let currentImage = document.querySelector(".slider__item_active");
+        activeDot.classList.toggle("slider__dot_active");
+        currentImage.classList.toggle("slider__item_active");
+        dotElems.item(k).classList.toggle("slider__dot_active");
+        imageElems.item(k).classList.toggle("slider__item_active");
+        return;
+    })
+}
